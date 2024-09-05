46e6616bb69452b6c20a0fe8e993573827a8b8d4
Max LC <167907843+MaxLC00@users.noreply.github.com>
2024-09-05 16:39:20 -0500

    Update server.js port
    update for render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const handlebarsHelpers = require('./helpers/handlebars-helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
 const hbs = exphbs.create({
     helpers: handlebarsHelpers
